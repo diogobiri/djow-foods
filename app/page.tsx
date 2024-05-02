1cdb700ffeb65e796189fd2d8e9730720cba7488
diogobiri <diogokglms@hotmail.com>
2024-05-02 10:46:46 -0300

    feat: add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Search from "./_components/ui/search-input";
 import Header from "./_components/ui/header";
 import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -12,6 +13,19 @@ const Home = () => {
 
       <div className="px-5 pt-6">
       <CategoryList/>
+      </div>
+
+      <div className="pt-6 px-5">
+      <Image 
+        src="/banner.png" 
+        alt="Até 30% desconto em pizzas" 
+        height={0} 
+        width={0} 
+        className="h-auto w-full object-contain" 
+        sizes="100vw"
+        quality={100}
+
+      />
       </div>
     </>
     );
